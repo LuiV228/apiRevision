43a2db5b258b997bb6017b8d2f5b63ad8eeb3982
LuiV228 <luqmaan.toffey@gmail.com>
2024-02-07 10:06:21 +0200

    addition

--- addition.js
+++ addition.js
@@ -1,1 +1,13 @@
-console.log('Addition');
+// For exporting one function quickly
+exports.addition = (a, b)=>{
+    return a + b
+}
+
+// For exporting multiple functions at once
+// function addition(a, b) {
+//     return a + b
+// }
+
+// module.exports = {
+//     addition
+// }
